48b09b1fb797e8d33d0ef48ddd964cda5d56691f
Rick Flagg <richard.lee.dlagg@outlook.com>
2022-12-18 17:36:13 -0500

    Adding required client libraries

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():  # put application's code here
-    return 'Hello World!'
+def index(prms=None):  # put application's code here
+    return render_template('index.html', prms=prms)
 
 
 if __name__ == '__main__':
