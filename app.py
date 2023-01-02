b2b335ba17fd7a45424fce0f94aa1cf2e7d1ec45
Rick Flagg <richard.lee.flagg@outlook.com>
2023-01-01 20:36:49 -0500

    Adding sqlalchemy mappings

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+import os
+
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
-db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///{}/app.db'.format(os.path.dirname(__file__))
 
+
+db = SQLAlchemy(app)
 
 
 class Task(db.Model):
@@ -15,17 +21,36 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=True)
-
+    tags = relationship("Tag", back_populates="task")
+    documents = relationship("Document", back_populates="task")
 
 class Tag(db.Model):
-    __tablename__ = "tasks"
+    __tablename__ = "task_tags"
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.String, nullable=False)
+    task_id = db.Column(db.Integer, ForeignKey('tasks.id'))
+    task = relationship("Task", back_populates="tags")
+
+
+class Document(db.Model):
+    __tablename__ = "task_documents"
+    id = db.Column(db.Integer, primary_key=True)
+    mimetype = db.Column(db.String, nullable=False)
+    upload_filename = db.Column(db.String, nullable=False)
+    generated_filename = db.Column(db.String, nullable=False)
+    task_id = db.Column(db.Integer, ForeignKey('tasks.id'))
+    task = relationship("Task", back_populates="documents")
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def index(prms=None):  # put application's code here
-    return render_template('index.html', prms=prms)
+    print(db.session.execute(db.select(Task)))
+    tasks = db.session.execute(db.select(Task))
+    return render_template('index.html', tasks=tasks)
 
 
 @app.route('/api/updateTask', methods=['POST'])
